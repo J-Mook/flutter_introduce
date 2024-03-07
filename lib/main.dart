db88f420568301679b9132aa377155da848e7a6d
jmook <jwonmook@gmail.com>
2024-03-08 00:34:31 +0900

    💄 서브페이지 생성, 메인페이지 배경

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,105 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      title: "jmook",
+      initialRoute: '/',
+      routes: {
+        '/' : (context) => MainPage(),
+        '/home' : (context) => MainPage(),
+        '/sub' : (context) => Subpage(),
+      },
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("J-Mook"),
       ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Flexible(
+              fit: FlexFit.tight,
+              flex: 2,
+              child: Container(
+                child: Container(
+                  color: Colors.amber,
+                  // child: Text("This is Home"),
+                ),
+              ),
+            ),
+            Flexible(
+              fit: FlexFit.tight,
+              flex: 1,
+              child: Container(
+                child: Container(
+                  color: Colors.brown,
+                  // child: Text("This is Home"),
+                ),
+              ),
+            ),
+          ],
+        )
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            Navigator.pushNamed(context, '/sub');
+          });
+        },
+        child: Icon(Icons.link),
+      ),
+    );
+  }
+}
+
+class Subpage extends StatefulWidget {
+  const Subpage({super.key});
+
+  @override
+  State<Subpage> createState() => _SubpageState();
+}
+
+class _SubpageState extends State<Subpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Text("asdf"),
+      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: () {
+      //     Navigator.pushNamed(context, '/home');
+      //   },
+      //   child: Icon(Icons.home),
+      // ),
     );
   }
 }
